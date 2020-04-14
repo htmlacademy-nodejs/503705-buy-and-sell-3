edbd312a09ddd7a78f7082caaa4383ccdc0f7c8d
Vasilisa <evstigney2010@gmail.com>
2020-04-14 15:11:01 +0300

    сделала шаблоны

--- src/express/index.js
+++ src/express/index.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 const app = express();
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const offersRoutes = require(`./routes/offers-routes`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
